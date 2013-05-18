0fb93f817ea083fe0f90413002d1d610b1253b69
Think <thin37421@gmail.com>
2013-05-18 21:54:00 +0800

    优化内容大幅超出限制时的性能表现

--- jquery.ellipsis.js
+++ jquery.ellipsis.js
@@ -3,31 +3,48 @@
 
 (function ($) {
     function ellipsis($elem, options) {
-        var max = options.maxWidth;
+        var max = options.maxWidth,
+    		ellipsis_char = options.ellipsisChar;
         if (!max) {
             max = $elem.width();
         }
         max = max * options.maxLine;
-        var text = $.trim($elem.text()).replace(' ','　');　
+        var text = $.trim($elem.text()).replace(' ','　'); //for fix white-space bug　
         var $temp_elem = $elem.clone(false)
             .css({ "visibility": "hidden", "whiteSpace": "nowrap","width":"auto" })
             .appendTo(document.body);
         var width = $temp_elem.width();
-        var stop = text.length;
-        while (width > max && stop > 1) {
-            stop--;
-            var temp_str = text.substring(0, stop) + ellipsis_char;
-            width = $temp_elem.text(temp_str).width();
-        }
-        $elem.text($temp_elem.text().replace('　',' '));
+		if(width > max){
+			var stop =  Math.floor(text.length * max / width); // for performance while content exceeding the limit substantially
+			var temp_str = text.substring(0,stop) + ellipsis_char;
+			width = $temp_elem.text(temp_str).width();
+			if(width > max){
+				while (width > max && stop > 1) {
+					stop--;
+					temp_str = text.substring(0, stop) + ellipsis_char;
+					width = $temp_elem.text(temp_str).width();
+				}					
+			}
+			else if(width < max){
+				while (width < max && stop < text.length) {
+					stop++;
+					temp_str = text.substring(0, stop) + ellipsis_char;
+					width = $temp_elem.text(temp_str).width();
+				}
+				if(width > max){
+					temp_str = text.substring(0,stop -1)+ellipsis_char;
+				}
+			}
+			$elem.text(temp_str.replace('　',' '));
+		}
         $temp_elem.remove();
     }
 
-    var ellipsis_char = '...';
 
     var defaults = {
         maxWidth: 0,
-        maxLine: 1
+        maxLine: 1,
+		ellipsisChar:'...'
     };
 
     $.fn.ellipsis = function (options) {
